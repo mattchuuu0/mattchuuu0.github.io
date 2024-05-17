1ebf8456e1b0b04c322b738462f683ae4b8b2898
TorresJP19 <TorresJP19.github.io>
2024-05-17 15:32:09 +0845

    update

---
+++ serviceworker.js
@@ -0,0 +1,22 @@
+const urlsToCache = [
+    // List of assets to cache.
+    "/",
+    "images/apple-touch-icon.png",
+    "images/favicon.ico",
+    "images/icon-192-maskable.png",
+    "images/icon-192.png",
+    "images/icon-512-maskable.png",
+    "images/icon-512.png"
+  ];
+   
+  self.addEventListener("install", (event) => {
+    let cacheUrls = async () => {
+       const cache = await caches.open("pwa-assets");
+       return cache.addAll(urlsToCache);
+    };
+    event.waitUntil(cacheUrls());
+  });
+   
+  self.addEventListener("fetch", event => {
+    event.respondWith(caches.match(event.request));
+  });
